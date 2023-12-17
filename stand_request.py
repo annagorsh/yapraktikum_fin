6d4420267f5324846a6c21908e2cc1f65f50b125
annagorsh <annagorshhh@gmail.com>
2023-12-17 16:53:43 +0300

    after review

--- stand_request.py
+++ stand_request.py
@@ -1,18 +1,10 @@
 import configuration
 import requests
-import data
 
 def create_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
                          json=body)
 
-response = create_order(data.order_body)
-tracking_code = response.json()
-track = tracking_code['track']
-
-def get_info_by_track():
+def get_info_by_track(track_number):
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK_PATH,
-                        params={"t":track})
-
-
-
+                        params={"t":track_number})
--- test_findbytrack.py
+++ test_findbytrack.py
@@ -1,5 +1,10 @@
-import stand_request
+# Анна Горшенкова, 11-я когорта — Финальный проект. Инженер по тестированию плюс
 
+import stand_request
+import data
 def test_find_order_by_track():
-    we_got_info_response = stand_request.get_info_by_track()
-    assert we_got_info_response.status_code == 200
+    create_response = stand_request.create_order(data.order_body)
+    tracking_code = create_response.json()
+    track = tracking_code['track']
+    final_response = stand_request.get_info_by_track(track)
+    assert final_response.status_code == 200
